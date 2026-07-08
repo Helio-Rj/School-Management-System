ce385fc3b40a79a3d849b05789ceed0f3cc42052
Hélio-Rj <hedonasi.1979@gmail.com>
2026-07-08 20:08:18 -0300

    feat(courses): implement Course model, admin registration and dynamic course listing

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+
+    active = models.BooleanField(
+        default=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+
+    workload = models.PositiveIntegerField(
+        default=0
+    )
+
+    def __str__(self):
+        return self.name
