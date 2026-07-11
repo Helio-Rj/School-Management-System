9eb39d6d93db5b2062b84453b46a046be04d2c31
Hélio-Rj <hedonasi.1979@gmail.com>
2026-07-10 23:38:02 -0300

    feat: implement teacher module and first database relationship

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-
 from .models import Student
 from .models import Course
+from .models import Teacher
 
 admin.site.register(Student)
 admin.site.register(Course)
+admin.site.register(Teacher)
